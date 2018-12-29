7e0c29522b2054128664bbc9148b753e22b16cfa
Talal <nweirant@gmail.com>
2018-12-29 12:20:05 -0500

    renders correctly

--- react-client/src/components/protectionPlan.jsx
+++ react-client/src/components/protectionPlan.jsx
@@ -1,19 +1,38 @@
 import React from 'react';
 
-const ProtectionPlan = ({twoYear, fourYear, isElectronic}) => {
-  if (isElectronic) {
-  return (
-    <div className="protection">
-    <b>Add a Protection Plan: </b>
-      <form>
-      <input type="checkbox" name="fourYear" className="check" /> <a href="#">4-Year Protection</a> for <span className="priceNoSale"> ${fourYear}</span><br />
-      <input type="checkbox" name="twoYear" className="check" /> <a href="#">2-Year Protection</a> for <span className="priceNoSale"> ${twoYear}</span><br />
-      </form>
-    </div>
-    )
+class ProtectionPlan extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      four : false,
+      two : false
+    }
+    this.toggleChecked = this.toggleChecked.bind(this);
   }
-  else {
-    return null;
+
+  toggleChecked(event) {
+    console.log(event.target.name);
+
+  }
+
+  render() {
+    if (this.props.isElectronic) {
+    return (
+    <div className="protection">
+      <b>Add a Protection Plan: </b>
+      <div>
+        <input type="radio" id="four" name="drone" value="four" />
+        <label for="four"><a href="#">4-Year Protection</a><span> for </span> <span className="priceNoSale">${this.props.fourYear}</span></label>
+      </div>
+
+      <div>
+        <input type="radio" id="two" name="drone" value="two" />
+        <label for="two"><a href="#">2-Year Protection</a><span> for </span> <span className="priceNoSale">${this.props.twoYear}</span></label>
+      </div>
+    </div>
+      )
+    }
+    else { return null; }
   }
 }
 
