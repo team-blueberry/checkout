139aed41650d2de1a83a24b1429a8af463db94c1
Talal <nweirant@gmail.com>
2018-12-29 20:00:05 -0500

    fixed css bug in shipTo

--- react-client/src/components/shipTo.jsx
+++ react-client/src/components/shipTo.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const ShipTo = ({name, city, zip}) => {
   return (
     <div>
-    <span className="shippingTo"> <a href="#">Deliver to {name} - {city} {zip} </a> </span> <hr />
+    <span className="shippingTo">Deliver to {name} - {city} {zip}</span> <hr />
     </div>
   )
 }
