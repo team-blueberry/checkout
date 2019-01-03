2b41dc7375dfdf49c5eb3f70759bea3fe8b4cc3c
Talal <nweirant@gmail.com>
2019-01-03 17:17:13 -0500

    bug fixed

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const compression = require('compression');
 app.use(express.static(__dirname + '../react-client/dist/'));
 app.use(bodyParser.json());
 
-app.use(compression());
+//app.use(compression());
 
 // app.get('/', (req,res) => {
 //
